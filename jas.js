43bd816c5d6ecaff8d1b5cb9e433310a56c15322
aashi9565 <pallavishukla191@gmail.com>
2024-08-30 11:28:13 +0530

    first commit

---
+++ jas.js
@@ -0,0 +1,9 @@
+var x=` 
+        <div class="logo" >logo</div>
+        <ul>
+            <li><a href=" ">home</a></li>
+            <li><a href=" ">home</a></li>
+            <li><a href=" ">home</a></li>
+            <li><a href=" ">home</a></li>
+        </ul>`;
+        document.getElementById("navbar").innerHTML=x;
